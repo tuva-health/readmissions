bdce87f053c188cbbeeb077d6dc6d0f4ccb5b58a
tuvaforrest <forrest@tuvahealth.com>
2022-12-02 14:07:51 -0500

    updated logic

--- integration_tests/models/procedure.sql
+++ integration_tests/models/procedure.sql
@@ -1,3 +1,2 @@
-create or replace table core_readmissions_lean.procedure as
-    (select encounter_id, code, code_type
-     from  {{source('core','procedure')}})
+select encounter_id, code, code_type
+     from  {{source('core','procedure')}}
